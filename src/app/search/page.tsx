2390121a37ea771e84ad93edd84701144375bab6
Scholars-Space <marufquadri@gmail.com>
2025-08-27 01:41:30 +0100

    update

--- src/app/search/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllPosts } from '../../lib/markdown';
-import ClientSearchPage from './ClientSearchPage';
-
-export default function SearchPage() {
-  const allContent = getAllPosts(); // Server-side fetch
-  return <ClientSearchPage allContent={allContent} />;
-}
